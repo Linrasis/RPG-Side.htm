793c1ee4d3df96bdadd28f83bd2f51d6c8fc4f53
Jan Horava <czechinthemail@gmail.com>
2015-11-16 21:05:10 -0800

    added double quotes around step value

--- js/rpg-side.js
+++ js/rpg-side.js
@@ -793,7 +793,7 @@ function setmode(newmode, newgame){
 
     document.body.innerHTML = '<div><div><a onclick="setmode(1, true)">Test Level</a></div></div><div class=right><div><input disabled value=Click>Cast Spell<br><input id=jump-key maxlength=1 value='
       + settings['jump-key'] + '>Jump<br><input disabled value=ESC>Main Menu<br><input id=movement-keys maxlength=2 value='
-      + settings['movement-keys'] + '>Move ←→<br><input disabled value="0 - 9">Select Spell</div><hr><div><input id=audio-volume max=1 min=0 step=.01 type=range value='
+      + settings['movement-keys'] + '>Move ←→<br><input disabled value="0 - 9">Select Spell</div><hr><div><input id=audio-volume max=1 min=0 step=".01" type=range value='
       + settings['audio-volume'] + '>Audio<br><input id=color type=color value='
       + settings['color'] + '>Color<br><input id=ms-per-frame value='
       + settings['ms-per-frame'] + '>ms/Frame<br><a onclick=reset()>Reset Settings</a></div></div>';
