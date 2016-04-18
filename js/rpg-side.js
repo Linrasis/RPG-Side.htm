3cde22441a549b1f39363f439e00897c139695ea
honzi <czechinthemail@gmail.com>
2016-04-17 18:36:21 -0700

    temporarily removed play_audio()

--- js/rpg-side.js
+++ js/rpg-side.js
@@ -696,15 +696,6 @@ function mouse_wheel(e){
     );
 }
 
-function play_audio(id){
-    if(settings['audio-volume'] <= 0){
-        return;
-    }
-
-    document.getElementById(id).currentTime = 0;
-    document.getElementById(id).play();
-}
-
 function reset(){
     if(!window.confirm('Reset settings?')){
         return;
