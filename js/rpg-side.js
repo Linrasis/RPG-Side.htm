56012012109b8cb179561211524b5ff6616f0c68
honzi <czechinthemail@gmail.com>
2016-05-15 01:50:34 -0700

    added update_settings() usage

--- js/rpg-side.js
+++ js/rpg-side.js
@@ -362,15 +362,20 @@ function setmode_logic(newgame){
 
     // Main menu mode.
     if(mode === 0){
-        document.body.innerHTML = '<div><div><a onclick="setmode(1, true)">Test Level</a></div></div><div class=right><div><input disabled value=Click>Cast Spell<br><input id=character-key maxlength=1 value='
-          + settings['character-key'] + '>Character Info<br><input id=inventory-key maxlength=1 value='
-          + settings['inventory-key'] + '>Inventory<br><input id=jump-key maxlength=1 value='
-          + settings['jump-key'] + '>Jump<br><input disabled value=ESC>Main Menu<br><input id=movement-keys maxlength=2 value='
-          + settings['movement-keys'] + '>Move ←→<br><input disabled value="0 - 9">Select Spell<br><input id=spellbook-key maxlength=1 value='
-          + settings['spellbook-key'] + '>Spellbook</div><hr><div><input id=audio-volume max=1 min=0 step=0.01 type=range value='
-          + settings['audio-volume'] + '>Audio<br><input id=color type=color value='
-          + settings['color'] + '>Color<br><input id=ms-per-frame value='
-          + settings['ms-per-frame'] + '>ms/Frame<br><a onclick=reset()>Reset Settings</a></div></div>';
+        document.body.innerHTML = '<div><div><a onclick="setmode(1, true)">Test Level</a></div></div>'
+          + '<div class=right><div><input disabled value=Click>Cast Spell<br>'
+          + '<input id=character-key maxlength=1>Character Info<br>'
+          + '<input id=inventory-key maxlength=1>Inventory<br>'
+          + '<input id=jump-key maxlength=1>Jump<br>'
+          + '<input disabled value=ESC>Main Menu<br>'
+          + '<input id=movement-keys maxlength=2>Move ←→<br>'
+          + '<input disabled value="0 - 9">Select Spell<br>'
+          + '<input id=spellbook-key maxlength=1>Spellbook</div><hr>'
+          + '<div><input id=audio-volume max=1 min=0 step=0.01 type=range>Audio<br>'
+          + '<input id=color type=color>Color<br>'
+          + '<input id=ms-per-frame>ms/Frame<br>'
+          + '<a onclick=reset()>Reset Settings</a></div></div>';
+        update_settings();
 
     // New game mode.
     }else{
